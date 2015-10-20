5b5e941106e63b5aa6b644be66bee91b7677ca18
Rizwan Merchant <rizwan@digitalphoenixproductions.com>
2015-10-20 13:32:49 -0500

    error handling added for login

--- Android/TechneauxMobileApp/app/src/main/java/com/techneaux/techneauxmobileapp/MainActivity.java
+++ Android/TechneauxMobileApp/app/src/main/java/com/techneaux/techneauxmobileapp/MainActivity.java
@@ -1,17 +1,64 @@
 package com.techneaux.techneauxmobileapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static Button submitLoginInfo;
+    private static EditText username;
+    private static EditText password;
+    private static TextView CSNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitLoginInfo = (Button) findViewById(R.id.loginSubmit);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        CSNumber = (TextView) findViewById(R.id.CSNumberText);
+
+
+
+
+        submitLoginInfo.setOnClickListener(new Button.OnClickListener() {
+
+            public void onClick(View v) {
+               String sUsername = username.getText().toString();
+               String sPassword = password.getText().toString();
+                if(sUsername.matches(""))
+                {
+                    Toast.makeText(getApplicationContext(), "Username not entered!",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else if(sPassword.matches(""))
+                {
+                    Toast.makeText(getApplicationContext(), "Password not entered!",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Username: " + sUsername + " Password: " + sPassword,
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+
+            }
+        });
+
     }
 
     @Override
